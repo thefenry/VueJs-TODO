ad4a771a12bdf0df398e1c4f7b9e6d3ba758e97e
Ivan Sued <thefenry@gmail.com>
2018-11-28 18:39:00 -0800

    Add functionality to add items

--- Todo-List/components/app.js
+++ Todo-List/components/app.js
@@ -8,8 +8,19 @@ var todoApp = new Vue({
   data: {
     todoList: [
       { id: 1, task: 'Learn Vue.js' },
-      { id: 1, task: 'Discuss it' },
-      { id: 1, task: 'compare Vue.js' }
-    ]
+      { id: 2, task: 'Discuss it' },
+      { id: 3, task: 'compare Vue.js' }
+    ],
+    newItem: ''
+  },
+  methods: {
+    addToList: function () {
+      
+      var lastItemId = this.todoList[this.todoList.length -1].id;
+      newId = lastItemId + 1;
+      
+      this.todoList.push({id: newId, task: this.newItem});
+      this.newItem = '';
+    }
   }
 });
